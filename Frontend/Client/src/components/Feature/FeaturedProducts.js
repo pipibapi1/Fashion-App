d3375671c86bf6cfaff094a03f8291f4466ed66b
AoNoAdmiral <90148066+AoNoAdmiral@users.noreply.github.com>
2021-10-31 00:32:05 +0700

    Ok
    I dont know

--- Frontend/Client/src/components/Feature/FeaturedProducts.js
+++ Frontend/Client/src/components/Feature/FeaturedProducts.js
@@ -1,183 +1,65 @@
-import React from 'react'
+import React,{useState} from 'react'
+const products = [
+    {1: "product name",2: "image/product_img1.jpg", 3: "$20.00", 4: "$25.00", 5: "20% off", 6: "(50)"},
+    {1: "product name",2: "image/product_img2.jpg", 3: "$20.00", 4: "$25.00", 5: "20% off", 6: "(50)"},
+    {1: "product name",2: "image/product_img3.jpg", 3: "$20.00", 4: "$25.00", 5: "20% off", 6: "(50)"},
+    {1: "product name",2: "image/product_img4.jpg", 3: "$20.00", 4: "$25.00", 5: "20% off", 6: "(50)"},
+    {1: "product name",2: "image/product_img5.jpg", 3: "$20.00", 4: "$25.00", 5: "20% off", 6: "(50)"},
+    {1: "product name",2: "image/product_img6.jpg", 3: "$20.00", 4: "$25.00", 5: "20% off", 6: "(50)"}
+];
 
 function FeaturedProducts() {
+    var index = -1;
+    var current = 0;
+    const a=()=>{
+        if(current==index) {current=-1;}
+        current++;
+        setActive(current);
+    }
+    const b=()=>{
+        if(current==0) {current=6;}
+        current--;
+        setActive(current);
+    }
+    const [active,setActive]=useState(0);
     return (
         <div className="swiper featured-slider">
-        
-        <div className="swiper-wrapper">
-
-            <div className="swiper-slide slide">
-                <div className="icons">
-                    <a href="#" className="fas fa-shopping-cart"></a>
-                    <a href="#" className="fas fa-heart"></a>
-                    <a href="#" className="fas fa-search"></a>
-                    <a href="#" className="fas fa-eye"></a>
-                </div>
-                <div className="image">
-                    <img src="image/product_img1.jpg" alt=""/>
-                </div>
-                <div className="content">
-                    <h3>product name</h3>
-                    <div className="price">
-                        <div className="amount">$20.00</div>
-                        <div className="cut">$25.00</div>
-                        <div className="offer">20% off</div>
-                    </div>
-                    <div className="stars">
-                        <i className="fas fa-star"></i>
-                        <i className="fas fa-star"></i>
-                        <i className="fas fa-star"></i>
-                        <i className="fas fa-star"></i>
-                        <i className="far fa-star"></i>
-                        <span>(50)</span>
-                    </div>
-                </div>
-            </div>
-
-            <div className="swiper-slide slide">
-                <div className="icons">
-                    <a href="#" className="fas fa-shopping-cart"></a>
-                    <a href="#" className="fas fa-heart"></a>
-                    <a href="#" className="fas fa-search"></a>
-                    <a href="#" className="fas fa-eye"></a>
-                </div>
-                <div className="image">
-                    <img src="image/product_img2.jpg" alt=""/>
-                </div>
-                <div className="content">
-                    <h3>product name</h3>
-                    <div className="price">
-                        <div className="amount">$20.00</div>
-                        <div className="cut">$25.00</div>
-                        <div className="offer">20% off</div>
-                    </div>
-                    <div className="stars">
-                        <i className="fas fa-star"></i>
-                        <i className="fas fa-star"></i>
-                        <i className="fas fa-star"></i>
-                        <i className="fas fa-star"></i>
-                        <i className="far fa-star"></i>
-                        <span>(50)</span>
-                    </div>
-                </div>
-            </div>
-
-            <div className="swiper-slide slide">
-                <div className="icons">
-                    <a href="#" className="fas fa-shopping-cart"></a>
-                    <a href="#" className="fas fa-heart"></a>
-                    <a href="#" className="fas fa-search"></a>
-                    <a href="#" className="fas fa-eye"></a>
-                </div>
-                <div className="image">
-                    <img src="image/product_img3.jpg" alt=""/>
-                </div>
-                <div className="content">
-                    <h3>product name</h3>
-                    <div className="price">
-                        <div className="amount">$20.00</div>
-                        <div className="cut">$25.00</div>
-                        <div className="offer">20% off</div>
-                    </div>
-                    <div className="stars">
-                        <i className="fas fa-star"></i>
-                        <i className="fas fa-star"></i>
-                        <i className="fas fa-star"></i>
-                        <i className="fas fa-star"></i>
-                        <i className="far fa-star"></i>
-                        <span>(50)</span>
-                    </div>
-                </div>
-            </div>
-
-            <div className="swiper-slide slide">
-                <div className="icons">
-                    <a href="#" className="fas fa-shopping-cart"></a>
-                    <a href="#" className="fas fa-heart"></a>
-                    <a href="#" className="fas fa-search"></a>
-                    <a href="#" className="fas fa-eye"></a>
-                </div>
-                <div className="image">
-                    <img src="image/product_img4.jpg" alt=""/>
-                </div>
-                <div className="content">
-                    <h3>product name</h3>
-                    <div className="price">
-                        <div className="amount">$20.00</div>
-                        <div className="cut">$25.00</div>
-                        <div className="offer">20% off</div>
-                    </div>
-                    <div className="stars">
-                        <i className="fas fa-star"></i>
-                        <i className="fas fa-star"></i>
-                        <i className="fas fa-star"></i>
-                        <i className="fas fa-star"></i>
-                        <i className="far fa-star"></i>
-                        <span>(50)</span>
-                    </div>
-                </div>
-            </div>
-
-            <div className="swiper-slide slide">
-                <div className="icons">
-                    <a href="#" className="fas fa-shopping-cart"></a>
-                    <a href="#" className="fas fa-heart"></a>
-                    <a href="#" className="fas fa-search"></a>
-                    <a href="#" className="fas fa-eye"></a>
-                </div>
-                <div className="image">
-                    <img src="image/product_img5.jpg" alt=""/>
-                </div>
-                <div className="content">
-                    <h3>product name</h3>
-                    <div className="price">
-                        <div className="amount">$20.00</div>
-                        <div className="cut">$25.00</div>
-                        <div className="offer">20% off</div>
-                    </div>
-                    <div className="stars">
-                        <i className="fas fa-star"></i>
-                        <i className="fas fa-star"></i>
-                        <i className="fas fa-star"></i>
-                        <i className="fas fa-star"></i>
-                        <i className="far fa-star"></i>
-                        <span>(50)</span>
-                    </div>
-                </div>
-            </div>
-
-            <div className="swiper-slide slide">
-                <div className="icons">
-                    <a href="#" className="fas fa-shopping-cart"></a>
-                    <a href="#" className="fas fa-heart"></a>
-                    <a href="#" className="fas fa-search"></a>
-                    <a href="#" className="fas fa-eye"></a>
-                </div>
-                <div className="image">
-                    <img src="image/product_img6.jpg" alt=""/>
-                </div>
-                <div className="content">
-                    <h3>product name</h3>
-                    <div className="price">
-                        <div className="amount">$20.00</div>
-                        <div className="cut">$25.00</div>
-                        <div className="offer">20% off</div>
-                    </div>
-                    <div className="stars">
-                        <i className="fas fa-star"></i>
-                        <i className="fas fa-star"></i>
-                        <i className="fas fa-star"></i>
-                        <i className="fas fa-star"></i>
-                        <i className="far fa-star"></i>
-                        <span>(50)</span>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-
-        <div className="swiper-button-next"></div>
-        <div className="swiper-button-prev"></div>
+        {products.map(function(props){index++;
+                return(
+                    (active==index)?<div className="swiper-wrapper">
+                        <div className="swiper-slide slide">
+                            <div className="icons">
+                                <a href="#" className="fas fa-shopping-cart"></a>
+                                <a href="#" className="fas fa-heart"></a>
+                                <a href="#" className="fas fa-search"></a>
+                                <a href="#" className="fas fa-eye"></a>
+                            </div>
+                            <div className="image">
+                                <img src={props[2]} alt=""/>
+                            </div>
+                            <div className="content">
+                                <h3>{props[1]}</h3>
+                                    <div className="price">
+                                        <div className="amount">{props[3]}</div>
+                                        <div className="cut">{props[4]}</div>
+                                        <div className="offer">{props[5]}</div>
+                                    </div>
+                                    <div className="stars">
+                                        <i className="fas fa-star"></i>
+                                        <i className="fas fa-star"></i>
+                                        <i className="fas fa-star"></i>
+                                        <i className="fas fa-star"></i>
+                                        <i className="far fa-star"></i>
+                                        <span>{props[6]}</span>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>:""
+                    )
+                
+            })}
+        <div className="swiper-button-next" onClick={a}></div>
+        <div className="swiper-button-prev" onClick={b}></div>
 
     </div>
     )
