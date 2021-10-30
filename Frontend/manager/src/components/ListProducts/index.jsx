6ab6c7fb5e856224f89db6d2bc83205b21df947b
hoangkui <hoangkui2001@gmail.com>
2021-10-30 14:39:03 +0700

    hoangkui lam phan danh sach san pham

--- Frontend/manager/src/components/ListProducts/index.jsx
+++ Frontend/manager/src/components/ListProducts/index.jsx
@@ -1,56 +1,68 @@
-import React from 'react'
-import "./index.css"
+import React from "react";
+import Product from "./Product";
+import "./index.css";
 const ListProducts = () => {
-    return (
-        <>
-            <div className="listProducts-heading">
-                <h3 className="listProducts-heading-title">
-                    Danh sách sản phẩm
-                </h3>
-                <input type="text" className="listProducts-heading-search" placeholder="Tìm kiếm sản phẩm và thương hiệu"/>
-                <button className="listProducts-heading-add-product">Thêm sản phẩm</button>
-                <div className="listProducts-heading-info">
-                    <h4 className="listProducts-heading-info-name">Nguyễn Trần Hoàng</h4>
-                    <img src="https://scontent.fdad2-1.fna.fbcdn.net/v/t1.6435-9/148352211_1315852945481787_1410223456476714730_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=UTvzr_H01LEAX9fT_8T&_nc_ht=scontent.fdad2-1.fna&oh=ad71bf3dd4b49191be326bd411384505&oe=61A30021" alt="" className="listProducts-heading-info-img" />
-                </div>
-            </div>
-            <div className="listProducts-table">
-                <div className="listProducts-table-heading">
-                    <input type="checkbox" className="listProducts-table-heading-checkbox" />
-                    <h4 className="listProducts-table-heading-stt">STT</h4>
-                    <div className="listProducts-table-heading-name">
-                        <h4 className="listProducts-table-heading-name-title">Thương hiệu</h4>
-                        <i className="listProducts-table-heading-name-sort"></i>
-                    </div>
-                    <div className="listProducts-table-heading-name">
-                        <h4 className="listProducts-table-heading-name-title">Thương hiệu</h4>
-                        <i className="listProducts-table-heading-name-sort"></i>
-                    </div>
-                    <div className="listProducts-table-heading-name">
-                        <h4 className="listProducts-table-heading-name-title">Thương hiệu</h4>
-                        <i className="listProducts-table-heading-name-sort"></i>
-                    </div>
-                    <div className="listProducts-table-heading-name">
-                        <h4 className="listProducts-table-heading-name-title">Thương hiệu</h4>
-                        <i className="listProducts-table-heading-name-sort"></i>
-                    </div>
-                    <div className="listProducts-table-heading-name">
-                        <h4 className="listProducts-table-heading-name-title">Thương hiệu</h4>
-                        <i className="listProducts-table-heading-name-sort"></i>
-                    </div>
-                    <button className="listProducts-table-heading-delete">Xóa</button>
+  return (
+    <>
+      <div className="listProducts-heading">
+        <h3 className="listProducts-heading-title">Danh sách sản phẩm</h3>
+        <input
+          type="text"
+          className="listProducts-heading-search"
+          placeholder="Tìm kiếm sản phẩm và thương hiệu"
+        />
+        <button className="listProducts-heading-add-product">
+          Thêm sản phẩm
+        </button>
+        <div className="listProducts-heading-info">
+          <h4 className="listProducts-heading-info-name">Nguyễn Trần Hoàng</h4>
+          <img
+            src="https://scontent.fdad2-1.fna.fbcdn.net/v/t1.6435-9/148352211_1315852945481787_1410223456476714730_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=UTvzr_H01LEAX9fT_8T&_nc_ht=scontent.fdad2-1.fna&oh=ad71bf3dd4b49191be326bd411384505&oe=61A30021"
+            alt=""
+            className="listProducts-heading-info-img"
+          />
+        </div>
+      </div>
+      <div className="listProducts-content">
+        <table className="listProducts-content-table">
+          <tr className="listProducts-content-row-heading-table listProducts-content-row">
+            <th className="listProducts-content-row-heading">
+              <input
+                type="checkbox"
+                className="listProducts-content-row-checkbox"
+              />
+            </th>
+            <th className="listProducts-content-row-heading">STT</th>
+            <th className="listProducts-content-row-heading">Tên sản phẩm</th>
+            <th className="listProducts-content-row-heading">Tên thương hiệu</th>
+            <th className="listProducts-content-row-heading">Số lượng còn</th>
+            <th className="listProducts-content-row-heading">
+              Số lượng đã bán
+            </th>
+            <th className="listProducts-content-row-heading">Giá</th>
+            <th className="listProducts-content-row-heading"> </th>
+            <th className="listProducts-content-row-heading">
+              <button className="listProducts-content-row-remove">Xóa</button>
+            </th>
+          </tr>
+          <Product/>
+          <Product/>
+          <Product/>
+          <Product/>
+          <Product/>
+          <Product/>
+          <Product/>
+          <Product/>
+          <Product/>
+          <Product/>
+          <Product/>
+          <Product/>
 
+          
+        </table>
+      </div>
+    </>
+  );
+};
 
-
-                </div>
-                <ul className="listProducts-list">
-                    <li className="listProducts-item">
-
-                    </li>
-                </ul>
-            </div>
-        </>
-    )
-}
-
-export default ListProducts
+export default ListProducts;
