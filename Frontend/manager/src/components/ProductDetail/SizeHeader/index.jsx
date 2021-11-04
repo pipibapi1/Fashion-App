f2d97d2fcc1ca6c285bb040ea7c2fa1e16be3c6f
hoangkui <hoangkui2001@gmail.com>
2021-11-04 20:51:39 +0700

    fix click product

---
+++ Frontend/manager/src/components/ProductDetail/SizeHeader/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const SizeHeader = () => {
+    return (
+        <>
+            <div className="table-heading">
+            <p className="table-heading-stt">STT</p>
+            <p className="table-heading-size">Size</p>
+            <p className="table-heading-color">Màu sắc</p>
+            <p className="table-heading-sale">Số lượng đã bán</p>
+            <p className="table-heading-remain">Số lượng còn</p>
+          </div>
+        </>
+    )
+}
+
+export default SizeHeader
