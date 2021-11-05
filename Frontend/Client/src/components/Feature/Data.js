812f41117f799a89bc1a44683eea4dd2f20b08d1
AoNoAdmiral <90148066+AoNoAdmiral@users.noreply.github.com>
2021-11-05 21:25:59 +0700

    A
    X

--- Frontend/Client/src/components/Feature/Data.js
+++ Frontend/Client/src/components/Feature/Data.js
@@ -1,4 +1,4 @@
-export const Data = [
+export var Data = [
     {1: "product name",2: "image/product_img1.jpg", 3: "$20.00", 4: "$25.00", 5: "20% off", 6: "(50)"},
     {1: "product name",2: "image/product_img2.jpg", 3: "$20.00", 4: "$25.00", 5: "20% off", 6: "(50)"},
     {1: "product name",2: "image/product_img3.jpg", 3: "$20.00", 4: "$25.00", 5: "20% off", 6: "(50)"},
