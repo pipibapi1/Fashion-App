370aaadde0d8db50b489bd280078b04bd459b833
hoangkui <hoangkui2001@gmail.com>
2021-11-16 23:54:31 +0700

    fix server.js

--- Backend/server.js
+++ Backend/server.js
@@ -25,41 +25,27 @@ mongoose
     }
   );
 
-
 const ClerkRoute = require("./Route/Clerk.router");
 const testRoute = require("./Route/test.router");
 const ProductRoute = require("./Route/Product.router");
-<<<<<<< HEAD
 const ProductItemRoute = require("./Route/ProductItem.router");
 const CustomerRoute = require("./Route/Customer.router");
+const OrderRoute = require("./Route/Order.router");
 app.use(cors());
-app.use("/product/", ProductRoute);
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use("/product/", ProductRoute);
 app.use(
   fileupload({
     useTempFiles: true,
   })
 );
-=======
-const ProductItem = require("./Route/ProductItem.router");
-const CustomerRoute = require('./Route/Customer.router')
-const OrderRoute = require('./Route/Order.router')
-app.use(cors());
 
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-app.use(fileupload({
-   useTempFiles:true
-})); 
-
-
-app.use('/clerk', ClerkRoute);
-app.use('/test', testRoute);
-app.use('/order', OrderRoute);
-
->>>>>>> 427cb3e0bd52d1dd417833c5e82c38fc811d9588
+app.use("/clerk", ClerkRoute);
+app.use("/test", testRoute);
+app.use("/order", OrderRoute);
+app.use("/productitem/", ProductItemRoute);
 
 app.use(
   session({
@@ -76,7 +62,6 @@ app.use(
 app.use("/public/", express.static("public"));
 app.use("/clerk", ClerkRoute);
 app.use("/test", testRoute);
-app.use("/productitem/", ProductItemRoute);
 app.use("/Customer", CustomerRoute);
 app.use(
   session({
