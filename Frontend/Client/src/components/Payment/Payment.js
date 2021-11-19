1f9e19b0e3c10d2a699de63d1ca71c94e1bd9501
AoNoAdmiral <90148066+AoNoAdmiral@users.noreply.github.com>
2021-11-19 17:15:44 +0700

    X
    A

--- Frontend/Client/src/components/Payment/Payment.js
+++ Frontend/Client/src/components/Payment/Payment.js
@@ -1,9 +1,20 @@
 import React,{useState,useEffect} from 'react'
 import {Data,Shipping} from './Data'
-
+import axios from 'axios';
 function Payment() {
     const [count, setCount] = useState(JSON.parse(sessionStorage.getItem("Order")));
     const [total, setTotal] = useState(0);
+    const [note, setNote] = useState("");
+    const [address, setAddress] = useState("");
+    const [POP,setPOP]=useState(0);
+    let listItem = []
+    let listnumber = []
+    function changeRoot(event){
+        setNote(event.target.value);
+    }
+    function changeAdd(event){
+        setAddress(event.target.value);
+    }
     function calcTotal(){
         var x = JSON.parse(sessionStorage.getItem('Order'))
         var y = 0;
@@ -35,6 +46,72 @@ function Payment() {
         }
         return;
       }
+      function buyItem(props,size){
+        var x = JSON.parse(sessionStorage.getItem('Order'))
+        for(const a in x){
+            let com = JSON.parse(x[a])
+          if (com[0] === props[0] && com[5]===size ){
+            com[4]++;
+            x[a] = JSON.stringify(com)
+            sessionStorage.setItem('Order',JSON.stringify(x));
+            return;
+          }
+        }
+        x.push(JSON.stringify({0:props[0],1:props[1],2:props[2],3:props[3],4:1,5:size}));
+        sessionStorage.setItem('Order',JSON.stringify(x));  
+      }
+      function lowerItem(props,size){
+        var x = JSON.parse(sessionStorage.getItem('Order'))
+        for(const a in x){
+            let com = JSON.parse(x[a])
+          if (com[0] === props[0] && com[5]===size ){
+            com[4]--;
+            if (com[4]!=0){
+            x[a] = JSON.stringify(com)
+            sessionStorage.setItem('Order',JSON.stringify(x));}
+            else removeItem(props);
+            return;
+          }
+        }
+        x.push(JSON.stringify({0:props[0],1:props[1],2:props[2],3:props[3],4:1,5:size}));
+        sessionStorage.setItem('Order',JSON.stringify(x));  
+      }
+    function submit(){
+      let x= JSON.parse(sessionStorage.getItem('Order'))
+      for(const a in x){
+          let props = JSON.parse(x[a]);
+          const findProduct = {idX: props[0],sizeX: props[5]}
+          const response = axios.post('http://localhost:3000/productitem', findProduct).then(
+              (res) => {
+                listItem.push(res.data.ID)
+          })
+      }
+        for(const a in x){
+            let com = JSON.parse(x[a]);
+            listnumber.push(com[4]);
+          }
+          let now = new Date();
+          setTimeout(() => {
+        const Order = {
+            id: "OR000"+Math.floor((Math.random() * 1000) + 1),
+            date: now,
+            address: address,
+            status: "Chờ Xác Nhận",
+            note: note,
+            listItemID: listItem,
+            listQuantity:listnumber,
+            customerAccountId: sessionStorage.getItem('id'),
+            totalPrice: total
+          }
+            const response = axios.post('http://localhost:3000/order', Order).then(
+                (res) => {
+                    alert("Success");
+                    setNote("");
+                    setAddress("");
+                    setTimeout(()=>window.location = "/",1000);
+            })
+          },1000);
+    }
     return (
     <section class="cart">
     <div class="cart_left">
@@ -50,25 +127,24 @@ function Payment() {
                 <div class="cart_items_details-top">
                     <span class="items_name">{props[1]}</span>
                     <span class="closeIcon" id="closeIcon">
-                        <i class="fas fa-times"></i>
+                        <i class="fas fa-times" onClick={()=>removeItem(props)}></i>
                     </span>
                 </div>
-                <span class="cart_items_details-size">Size: {"XL"}</span>
+                <span class="cart_items_details-size">Size: {props[5]}</span>
                 <div class="cart_items_details-bottom">
                     <span class="cart_items_details-price">${props[3]}</span>
                     <div class="controls">
-                        <button class="minusIcon" type="button">-</button>
+                        <button class="minusIcon" type="button" onClick={()=>lowerItem(props,props[5])}>-</button>
                         <span class="count" id="count">{props[4]}</span>
-                        <button class="plusIcon"  type="button">+</button>
+                        <button class="plusIcon"  type="button" onClick={()=>buyItem(props,props[5])}>+</button>
                     </div>
                 </div>
             </div>
         </div>)})}
     <div class="cartPromoDiv">
             <form>
-            <input type="text" id="fname" name="fname"  placeholder="Promo code"/>
+            <input type="text" id="fname" name="fname" width="50%"  placeholder="Note" onChange={changeRoot}/>
             </form>
-            <button class="cta" type="button">Apply</button>
         </div>
     </div>
     <div class="cart_right">
@@ -88,9 +164,35 @@ function Payment() {
             <span class="rowAmount">${total+20}</span>
         </div>
         <div class="btn">
-            <button type="button">Checkout</button>
+            <button type="button" onClick={()=>setPOP(1)}>Checkout</button>
         </div>
     </div>
+    <div class={POP?"left apply-form":"hide left apply-form"}>
+        <form action="#" class="payment">
+        <span class="closeIcon" className="closecon" onClick={()=>setPOP(0)}>
+            <i class="fas fa-times"></i>
+        </span>
+          <div class="form-inner">
+            <div class="form-group">
+              <label for="">SỐ THẺ</label>
+              <input type="text" placeholder="9404 3202 5794 3749" id="" />
+            </div>
+            <div class="form-group">
+                <label for="">TÊN IN TRÊN THẺ</label>
+                <input type="text" placeholder="NGUYEN VAN A" id="" />
+            </div>
+            <div class="form-group">
+                <label for="">NGÀY PHÁT HÀNH</label>
+                <input type="text" placeholder="05/14" id="" />
+              </div>
+            <div class="form-group">
+              <label for="">ĐỊA CHỈ NHẬN HÀNG</label>
+              <input type="text" placeholder="132/20 LÝ THƯỜNG KIỆT....." id="" onChange={changeAdd}/>
+            </div>
+          </div>
+          <button class="submit-btn1" id="apply-form-btn" onClick={()=>submit()}>Confirm</button>
+        </form>
+      </div>
 </section>
     )
 }
