5ec67216f591cddd91e2229892a5d14c20191178
quan.ta <quan.ta@geekup.vn>
2022-09-03 09:45:22 +0700

    adjust package.json for compabitility

--- Backend/configure.js
+++ Backend/configure.js
@@ -3,7 +3,7 @@
 module.exports = {
   DB:
     //"mongodb+srv://root:0MKmrFE6ygcu2HGA@cluster0-njlqc.mongodb.net/shopping_cart"
-    " mongodb+srv://pipibapi1:13111620@cluster0.zfknm.mongodb.net/NoName",
+    "mongodb+srv://pipibapi1:13111620@cluster0.zfknm.mongodb.net/NoName",
 
   JWT_SECRET: "F9qy&s)?4=33s%$2h#F~",
 };
